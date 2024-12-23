f86dceb70c5060787a0732c60c45110beb79a553
likhithreddy09 <likhith.reddy@lotusdew.in>
2024-12-23 07:33:54 -0800

    push-commit-11

--- src/components/Blogs/PostPage.tsx
+++ src/components/Blogs/PostPage.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ContentContainer from "../Layout/ContentContainer/ContentContainer";
